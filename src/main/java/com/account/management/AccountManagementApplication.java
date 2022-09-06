f08211561d736755cdf0cbf29fad2e5fbd7d9c9c
olutayopeter <olutayopeter2014@gmail.com>
2022-09-06 18:46:21 +0100

    Add files via upload

--- src/main/java/com/account/management/AccountManagementApplication.java
+++ src/main/java/com/account/management/AccountManagementApplication.java
@@ -1,1 +1,13 @@
+package com.account.management;
 
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AccountManagementApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AccountManagementApplication.class, args);
+	}
+
+}
